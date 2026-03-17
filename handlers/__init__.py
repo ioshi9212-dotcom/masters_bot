8406e76519b5bd940b204b84d3ab82e5aa853887
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-03-17 19:59:35 +1000

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,17 @@
 from aiogram import Dispatcher
 
-from handlers import booking, client, common, master, profile, waitlist
+from handlers.booking import router as booking_router
+from handlers.client import router as client_router
+from handlers.common import router as common_router
+from handlers.master import router as master_router
+from handlers.profile import router as profile_router
+from handlers.waitlist import router as waitlist_router
 
 
 def setup_handlers(dp: Dispatcher) -> None:
-    dp.include_router(common.router)
-    dp.include_router(master.router)
-    dp.include_router(client.router)
-    dp.include_router(profile.router)
-    dp.include_router(booking.router)
-    dp.include_router(waitlist.router)
+    dp.include_router(common_router)
+    dp.include_router(master_router)
+    dp.include_router(client_router)
+    dp.include_router(profile_router)
+    dp.include_router(booking_router)
+    dp.include_router(waitlist_router)
