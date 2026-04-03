51d47416ee1c28c58d4cd13540706053591f2299
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-04-03 20:59:19 +1000

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import aiosqlite
+import logging
 
 
 class Database:
@@ -8,7 +9,17 @@ class Database:
         self.path = path
 
     async def connect(self) -> aiosqlite.Connection:
+        logging.info(f"Подключение к БД: {self.path}")
+
         conn = await aiosqlite.connect(self.path)
+
         conn.row_factory = aiosqlite.Row
+
+        # Включаем внешние ключи
         await conn.execute("PRAGMA foreign_keys = ON;")
+
+        # Улучшение для стабильности SQLite
+        await conn.execute("PRAGMA journal_mode = WAL;")
+        await conn.execute("PRAGMA synchronous = NORMAL;")
+
         return conn
